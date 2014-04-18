8e2c0e3934f07525ebff0a254de0cb48fba29511
Carlos Valera <cookyt2@gmail.com>
2014-04-18 17:20:13 -0400

    Changed to use of unique_ptr for the pool to prevent deletion issues.

--- thread/rc/pool_manager.h
+++ thread/rc/pool_manager.h
@@ -26,19 +26,22 @@ class PoolManager {
 
   PoolManager(int number_pools)
       : number_pools_(number_pools)
+      , allocated_pools_(0)
       , pools_(new ManagedPool[number_pools_]) {}
 
   /**
    * Allocates a pool for thread-local use. Semantically, the manager owns all
-   * pools.
+   * pools. Method is not thread-safe.
    */
   DescriptorPool * get_pool();
+
+  const int number_pools_;
 
 
  private:
   struct ManagedPool {
     // TODO(carlos) use a pool object, or a pool pointer?
-    DescriptorPool *pool {nullptr};
+    std::unique_ptr<DescriptorPool> pool {nullptr};
 
     std::atomic<PoolElement *> safe_pool;
     std::atomic<PoolElement *> unsafe_pool;
@@ -49,7 +52,12 @@ class PoolManager {
   static_assert(sizeof(ManagedPool) == CACHE_LINE_SIZE,
       "Managed pools have to be cache aligned to prevent false sharing.");
 
-  int number_pools_;
+  /**
+   * Keeps track of how many pools have been allocated.
+   * TODO(carlos) should this be atomic?
+   */
+  int allocated_pools_;
+
   std::unique_ptr<ManagedPool[]> pools_;
 };
 
