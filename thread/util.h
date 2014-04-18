3cdb1fe3e818028ba0ac30963247a3e6a51d7afa
Carlos Valera <cookyt2@gmail.com>
2014-04-18 17:27:15 -0400

    Removed unused util file

--- thread/util.h
+++
@@ -1,7 +0,0 @@
-#ifndef UCF_THREAD_UTIL_H_
-#define UCF_THREAD_UTIL_H_
-
-// see: http://stackoverflow.com/questions/4851075
-#define UNUSED(x) (void)(sizeof((x), 0))
-
-#endif  // UCF_THREAD_UTIL_H_
