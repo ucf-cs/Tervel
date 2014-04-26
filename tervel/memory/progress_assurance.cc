780fe36b4baf50d1eab9dcf6c6c6b5f9cdb04b0b
Carlos Valera <cookyt2@gmail.com>
2014-04-26 17:16:03 -0400

    progress_assurance.* review comments up

--- tervel/memory/progress_assurance.cc
+++ tervel/memory/progress_assurance.cc
@@ -3,6 +3,7 @@
 namespace tervel {
 namespace memory {
 
+// REVIEW(carlos): extra paren
 void ProgressAssurance::p_try_to_help()) {
   // Internally, delay_count is incremented and set to 0 when ever HELP_DELAY
   // is reached
@@ -12,6 +13,14 @@ void ProgressAssurance::p_try_to_help()) {
     int help_id = tl_thread_info->help_id(num_threads_);
     OpRecord *op = op_table_[help_id].load();
     if (op != nullptr) {
+      // REVIEW(carlos): Don't bother trying to line up a line continuation;
+      //   just indent the next line by 2 indentation levels (4 spaces). When
+      //   splitting a line, prefer to leave an opening brace or operator at the
+      //   end of the previous line:
+      //     ... address = reinterpret_cast<...>(
+      //         &(...));
+      //   This gives a visual cue to the reader that the line is being
+      //   continued.
       std::atomic<void *> *address = reinterpret_cast<std::atomic<void *> *>
                                                         (&(op_table_[help_id]));
 
@@ -24,12 +33,27 @@ void ProgressAssurance::p_try_to_help()) {
   }
 }
 
+// REVIEW(carlos): Default parameters should go in method declaration (.h
+//   file) since they serve as documentation for what the default case is. Don't
+//   also put them in the .cc file because then changes to one file must always
+//   be manually mirrored.
+// REVIEW(carlos): Don't bother trying to line up broken lines, just move to
+//   next line and indent 2 indentation levels (4 spaces). Ex:
+//     void this_is_a_long_function_name(int long_argument_name_too=
+//         default_argument);
+//   I prefer no spaces around the `=' if it's a default parameter, but either
+//   way is fine.
 void ProgressAssurance::p_make_annoucment(OpRecord *op, int tid =
                                                     tl_thread_info.thread_id) {
   op_table_[tid].store(op);
   op->help_complete();
   op_table_[tid].store(nullptr);
 }
+// REVIEW(carlos): Excessive vertical whitespace at before closing of namespace.
+//   Please try to limit vertical whitespace when possible so that more code is
+//   visible on screen (rule of thumb: usually just 1 blank line for
+//   readability, 2 blank lines to mark the beginning of a logical section)
+//   Here, one blank line before the closing brackets suffice.
 
 
 
--- tervel/memory/progress_assurance.h
+++ tervel/memory/progress_assurance.h
@@ -10,12 +10,21 @@
 
 namespace tervel {
 namespace memory {
+// REVIEW(carlos): Suggest 1 blank line between namespace blocks declaration and
+//   beginning of code
 /**
  * This class is used to create Operation Records.
  * Operation records are designed to allow an arbitary thread to complete
  * some other thread's operation in the event that thread is unable to do so.
  * These objects are HP protected.
  */
+// REVIEW(carlos): There is no `namespace HP' (capital letters)
+// REVIEW(carlos): why is OpRecord a subclass of HPElement? shouldn't we keep
+//   the two classes seperate? Otherwise, you're explicitly saying that the
+//   progress assurance relies on hazard pointere'd memory (which is fine, but
+//   if that's the case, progress assurance should be in a new namespace).
+// REVIEW(carlos): There's an op_record.h file, too. One of these doesn't
+//   belong. Which is it?
 class OpRecord : public HP::HPElement {
  public:
   virtual ~OpRecord() {}
@@ -44,10 +53,22 @@ class ProgressAssurance {
  public:
   // Const used to delay an announcement
   static constexpr size_t MAX_FAILURES = 1;
+  // REVIEW(carlos): When adding comments, I prefer to put a blank line before
+  //   the beginning of a comment block. Otherwise, It looks, at a glance, that
+  //   the comment block could apply to the above code when it usually only
+  //   belongs to the below code.
+  // REVIEW(carlos): For documentation comments, use the javadoc style /** */
+  //   comments.
   // Const used to reduce the number of times a thread checks the table
   // Reduces memory loads at the cost of a higher upper bound
   static constexpr size_t HELP_DELAY = 1;
 
+  // REVIEW(carlos): Don't bother trying to line up broken lines, just move to
+  //   next line and indent 2 indentation levels (4 spaces). Ex:
+  //     void this_is_a_long_function_name(int long_argument_name_too=
+  //         default_argument);
+  //   I prefer no spaces around the `=' if it's a default parameter, but either
+  //   way is fine.
   explicit ProgressAssurance(int num_threads =
                                       tl_thread_info.shared_info_->num_threads)
       : num_threads_ {num_threads}
@@ -57,6 +78,13 @@ class ProgressAssurance {
    * This function checks at most one position in the op_table_ for an OPRecod
    * If one is found it will call its help_complete function.
    */
+  // REVIEW(carlos): See above comment on indenting broken lines.
+  // REVIEW(carlos): I tend to prefer top-level functions over static functions.
+  //   There isn't much difference between the two, and static functions force
+  //   you to write the class name everytime you want to call it. If the
+  //   function is very strongly tied to the class, then static functions may be
+  //   appropriate, but I'd suggest treating them as the exception, rather than
+  //   the rule.
   static void check_for_announcement(ProgressAssurance *progress_assuarance =
                       tl_thread_info.progress_assuarance) {
     progress_assuarance->p_check_for_announcement();
@@ -67,6 +95,10 @@ class ProgressAssurance {
    * @param op an OpRecord to complete
    * @return on return the OpRecord must be completed.
    */
+  // REVIEW(carlos): See above comments on indenting broken lines. Typically,
+  //   the "start the line with the comma" only applies to long argument lists
+  //   (like constructor initializers) to even out visual weight. On functions,
+  //   I'd put the comma on the arguemnt.
   static void make_announcement(OpRecord *op, int tid = tl_thread_info.thread_id
                                 , ProgressAssurance *progress_assuarance =
                                           tl_thread_info.progress_assuarance) {
@@ -86,6 +118,8 @@ class ProgressAssurance {
    * @return on return the OpRecord must be completed.
    */
 
+  // REVIEW(carlos): There shouldn't be a space between the documentation
+  //   comment and the function declarati
   void p_make_announcement(OpRecord *op, int tid = tl_thread_info.thread_id);
 
   /**
@@ -94,6 +128,8 @@ class ProgressAssurance {
    */
   std::unique_ptr<std::atomic<OpRecord *>[]> op_table_;
 
+  // REVIEW(carlos): For documentation comments, please use javadoc style /** */
+  //   comments
   // The number of threads that are using this operation table
   int num_threads_;
 
