3c2c2c4c6f49ea79a8afbac8f9f913bcce68ea87
Carlos Valera <cookyt2@gmail.com>
2014-04-28 20:53:52 -0400

    comments on pool_manager.*

--- tervel/memory/rc/pool_manager.cc
+++ tervel/memory/rc/pool_manager.cc
@@ -6,6 +6,8 @@ namespace tervel {
 namespace memory {
 namespace rc {
 
+// REVIEW(carlos): Allocation is dangerous if function is called twice with the
+//   same `pos' See notes on this function in the .h file
 DescriptorPool * PoolManager::get_pool(int pos) {
   DescriptorPool *pool = new DescriptorPool(pos, this);
   pools_[pos].pool.reset(pool);
--- tervel/memory/rc/pool_manager.h
+++ tervel/memory/rc/pool_manager.h
@@ -35,6 +35,7 @@ class PoolManager {
 
   explicit PoolManager(int number_pools)
       : number_pools_(number_pools)
+      // REVIEW(carlos): no such element allocated_pools_
       , allocated_pools_(0)
       , pools_(new ManagedPool[number_pools_]) {}
 
@@ -43,6 +44,19 @@ class PoolManager {
    * pools. Method is thread-safe iff each thread calls it with a unique pos,
    * the thread's ID satifies this
    */
+  // REVIEW(carlos): Initially, I had designed the semantics of get_pool to
+  //   count the number of pools it needs to allocate, and to allocate "on
+  //   demand" when the function was called. (In retrospect, a name like
+  //   allocate_pool would have been better). The way it's been changed is
+  //   dangerous because if it's called twice with the same index, the previous
+  //   pool is deleted, causing some thread to have a pointer to deleted memory
+  //   and causing a memory leak of the elements inside the pool. If you want it
+  //   to simply act as an accessor to a pool with a given index, then it's best
+  //   if each `pool' member in the ManagedPool array was simply constructed in
+  //   the constructor of this class, and that get_pool simply acted as an
+  //   accessor for the element, rather than trying to allocate it.
+  // REVIEW(carlos): compiler error, you have to include info.h for the forward
+  //   declaration of tl_thread_info
   DescriptorPool * get_pool(int pos = tl_thread_info.thread_id);
 
   const int number_pools_;
@@ -50,7 +64,10 @@ class PoolManager {
 
  private:
   struct ManagedPool {
+    // REVIEW(carlos): missing semi colon
     std::unique_ptr<DescriptorPool> pool {nullptr}
+    // REVIEW(carlos): how are these linked lists freed? Destructor should
+    // traverse the lists and delete each element from it for safety.
     std::atomic<PoolElement *> safe_pool {nullptr}
     std::atomic<PoolElement *> unsafe_pool {nullptr}
 
