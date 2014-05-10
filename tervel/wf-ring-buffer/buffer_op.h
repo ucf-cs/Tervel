6635670dcb2bd8d459e0775ef3cb057e5bf03240
Andrew <abarrington@knights.ucf.edu>
2014-05-10 17:00:31 -0400

    included new buffer_op header

---
+++ tervel/wf-ring-buffer/buffer_op.h
@@ -0,0 +1,38 @@
+#ifndef TERVEL_WFRB_BUFFEROP_H
+#define TERVEL_WFRB_BUFFEROP_H
+
+
+#include "tervel/util/progress_assurance.h"
+#include "tervel/wf-ring-buffer/elem_node.h"
+#include "tervel/wf-ring-buffer/wf_ring_buffer.h"
+
+#include <atomic>
+
+namespace tervel {
+namespace wf_ring_buffer {
+
+
+template<class T>
+class RingBuffer;
+template<class T>
+class ElemNode;
+
+/**
+ *
+ */
+template <class T>
+class BufferOp : public util::OpRecord {
+  public:
+    explicit BufferOp<T>(RingBuffer<T> *buffer)
+      : buffer_(buffer) {}
+
+  protected:
+    RingBuffer<T> *buffer_;
+    ElemNode<T> *node_;
+    static constexpr ElemNode<T> *FAILED = reinterpret_cast<ElemNode<T> *>(0x1L);
+};  // BufferOp class
+
+}  // namespace wf_ring_buffer
+}  // namespace tervel
+
+#endif  // TERVEL_WFRB_BUFFEROP_H
