e2fd3ff628c2d824c811e60f6103dac71e23ed18
Samuel J. Goff <samueljgoff@gmail.com>
2014-05-21 17:55:48 -0400

    added skeleton for wait free hash map

---
+++ tervel/wf-hash-map/Node.h
@@ -0,0 +1,81 @@
+#ifndef TERVEL_UTIL_NODE_H
+#define TERVEL_UTIL_NODE_H
+
+using namespace std;
+template <typename K, typename V>
+
+class Node {
+private:
+	K key;
+	V value;
+	Node *next;
+public:
+	Node(const K &key, const V &value) : key(key), value(value), next(NULL) {
+	}
+	PairNode(const K &key, const V &value) : key(key), value(value), next(NULL) {
+	}
+	ArrayNode(const K &key, const V &value) : key(key), value(value), next(NULL){
+	}
+	K getKey() const {
+		return key;
+	}
+	V getValue() const {
+		return value;
+	}
+
+
+};
+class PairNode {
+private:
+	K key;
+	V value;
+	Node *next;
+public:
+
+	bool isPairNode(){
+		return true;
+	}
+	bool isArrayNode(){
+		return false;
+	}
+};
+
+class ArrayNode {
+private:
+	K key;
+	V value
+	Node *next;
+public:
+
+	bool isPairNode(){
+		return false;
+	}
+	bool isArrayNode(){
+		return true;
+	}
+
+};
+class HashMap{
+private:
+	Node **table;
+public:
+	HashMap(){
+		table = new Node*[array_length];
+		for (int i = 0; i < array_length; i++){
+			table[i] = NULL;
+		}
+	}
+	int get(int key) {
+
+	}
+	void insert(int key, int value){
+
+	}
+	bool update(int key, int oldValue, int newValue){
+
+	}
+	bool remove(int key, int value){
+
+	}
+
+};
