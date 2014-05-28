4d23f73dab69b35fe5bfb888fdc51265683bff15
Andrew <atbarrington@gmail.com>
2014-05-27 22:51:26 -0400

    added accidentally removed files from test_buffers

---
+++ tervel/tests/ring_buffer/test_buffers/mcas_API.h
@@ -0,0 +1,37 @@
+#ifndef __mcas_buffer_h__
+#define __mcas_buffer_h__
+
+#include "mcas/WFRingBuffer.h"
+
+template<class T>
+class TestBuffer {
+ public:
+  TestBuffer(size_t capacity, size_t num_threads)) {
+    Init_RingBuffer_Memory_Management(num_threads);
+    INIT_THREAD_ID();
+    queue_ = new RingBuffer(capacity);
+  };
+
+  char * name() {
+    return "WF MCAS Buffer";
+  }
+
+  void attach_thread() {
+    INIT_THREAD_ID();
+  };
+
+  void detach_thread() {
+  };
+
+  bool enqueue(T val) {
+    return queue_->enqueue(reinterpret_cast<void *>(val));
+  };
+  bool dequeue(T &val) {
+    void * tval;
+    return queue_->dequeue(tval);
+  };
+
+ private:
+  RingBuffer *queue_;
+};
+#endif
