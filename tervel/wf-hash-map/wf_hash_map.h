cd4117198adf8df7b34437c1449486496b6a2d15
Samuel J. Goff <samueljgoff@gmail.com>
2014-06-17 23:53:43 -0400

    Fixed some whitespace issues

--- tervel/wf-hash-map/wf_hash_map.h
+++ tervel/wf-hash-map/wf_hash_map.h
@@ -16,9 +16,9 @@ class HashMap{
  public:
   HashMap(){
      table = new Node*[arrayLength];
-	   for (int i = 0; i < arrayLength; i++){
-	     table[i] = nullptr;
-	   }
+       for (int i = 0; i < arrayLength; i++){
+         table[i] = nullptr;
+       }
    }
    HashMap(){
 
@@ -27,47 +27,46 @@ class HashMap{
   int get(T Key) {
     //not 100% sure if hashKey(key) means to do Key.hash() or not.
     uint64_t hash = currHash = Key.hash();
-	 std::atomic<Node *> local = head;
-	int result = null;
-	for (int r = 0; r < keySize - arrayPow; r += arrayPow){
-	  int pos = hash&(arrayLength - 1);
-	  hash = hash << arrayPow;
-	  node = getNode(local, pos);
-	  if (isArrayNode(node))
-	    local = node;
-	  else if (node == null)
-		break;
-	  else {
-	    watch(node);
-		if (node != getNode(local, pos) {
-		  int failCount = 0;
+    std::atomic<Node *> local = head;
+    int result = null;
+    for (int r = 0; r < keySize - arrayPow; r += arrayPow){
+      int pos = hash&(arrayLength - 1);
+      hash = hash << arrayPow;
+      node = getNode(local, pos);
+      if (isArrayNode(node))
+      local = node;
+      else if (node == null)
+        break;
+      else {
+        watch(node);
+        if (node != getNode(local, pos) {
+          int failCount = 0;
           while (node != getNode(local, pos)) {
-		    node = getNode(local, pos);
-			watch(node);
-			failCount++;
-			if (failCount > maxFailCount) {
-			  markDataNode(local, pos);
-			  local = expandMap(local, pos, r);
-			  break;
-			}
-		  }
+            node = getNode(local, pos);
+            watch(node);
+            failCount++;
+            if (failCount > maxFailCount) {
+              markDataNode(local, pos);
+              local = expandMap(local, pos, r);
+              break;
+            }
+          }
           if (isArrayNode(node) {
-		    local = node;
-			continue;
-	      }
-		  else if (isMarked(node)) {
-		    local = expandMap(local, pos, r);
-			continue;
-		  }
-	   	  else if (node == null) {
-		    break;
-		  }
-		}
-		if(node -> hash == currHash)
-		  result = node -> value;
-		break;
-    
-	  }
+            local = node;
+            continue;
+          }
+          else if (isMarked(node)) {
+            local = expandMap(local, pos, r);
+            continue;
+          }
+          else if (node == null) {
+            break;
+          }
+        }
+        if(node -> hash == currHash)
+          result = node -> value;
+        break;
+      }
     }
     if (r >= keySize - arrayPow) {
       pos = hash&(arrayLength-1);
@@ -79,60 +78,60 @@ class HashMap{
   //CAS and markDataNode, isMarked, free not implemented yet.
   bool insert(T Key,int value){
     uint64_t hash = Key.hash();
-	std::atomic<Node *> insertThis = allocateNode(value,hash);
-	std::atomic<Node *> local = head;
-	for (int r=0; r < keySize - arrayPow; r+=arrayPow) {
-	  //pos is an int or is it uin64_t?
-	  int pos = hash&(arrayLength - 1);
-	  uint64_t hash = hash >> arrayPow;
-	  int failCount = 0;
-	  std::atomic<Node *> node = getNode(local, pos);
-	  while (true) {
-	    if (failCount > maxFailCount) {
-		  node = markDataNode(local, pos);
-		if (node == null)
-		  if ((node = CAS(local[pos], null, insertThis)) == null) {
-		    watch(null);
-			return true;
-		  }
-		if (isMarked(node)) 
-		  node = expandMap(local, pos, r);
-		if (isArrayNode(node)) {
-		  local = node;
-		  break;
-		else {
-		  watch(node);
-		  std::atomic<Node *> node2 = getNode(local, pos);
-		  if (node != node2) {
-		    failCount++;
-			node = node2;
-			continue;
-		  }
-		  else if (node -> hash == insertThis -> hash) {
-		    watch(null);
-			free(insertThis);
-			return false;
-		  }
-		  else {
-		    node = expandMap(local, pos, r);
-			if(isArrayNode(node)) {
-			  local = node;
-			  break;
-			else {
-			  failCount++;
-			}
-	      }
-		}
+    std::atomic<Node *> insertThis = allocateNode(value,hash);
+    std::atomic<Node *> local = head;
+    for (int r=0; r < keySize - arrayPow; r+=arrayPow) {
+      //pos is an int or is it uin64_t?
+      int pos = hash&(arrayLength - 1);
+      uint64_t hash = hash >> arrayPow;
+      int failCount = 0;
+      std::atomic<Node *> node = getNode(local, pos);
+      while (true) {
+        if (failCount > maxFailCount) {
+          node = markDataNode(local, pos);
+        if (node == null)
+          if ((node = CAS(local[pos], null, insertThis)) == null) {
+            watch(null);
+            return true;
+          }
+        if (isMarked(node)) 
+          node = expandMap(local, pos, r);
+        if (isArrayNode(node)) {
+          local = node;
+          break;
+        else {
+          watch(node);
+          std::atomic<Node *> node2 = getNode(local, pos);
+          if (node != node2) {
+            failCount++;
+            node = node2;
+            continue;
+          }
+          else if (node -> hash == insertThis -> hash) {
+            watch(null);
+            free(insertThis);
+            return false;
+          }
+          else {
+            node = expandMap(local, pos, r);
+            if(isArrayNode(node)) {
+              local = node;
+              break;
+            else {
+              failCount++;
+            }
+          }
+        }
       }
-	}
-	free(insertThis);
-	watch(null);
-	pos = hash&(arrayLength - 1);
-	int currValue = local[pos];
-	if (currValue == null) 
-	  return (CAS(local[pos], null, value) == null);
-	else
-	  return false;
+    }
+    free(insertThis);
+    watch(null);
+    pos = hash&(arrayLength - 1);
+    int currValue = local[pos];
+    if (currValue == null) 
+      return (CAS(local[pos], null, value) == null);
+    else
+      return false;
   }
   bool update(T Key,int oldValue,int newValue){
 
@@ -150,7 +149,7 @@ class HashMap{
   
   std::atomic<Node *> allocateNode(int value, uint64_t hash) {
     //WIP
-	return node;
+    return node;
   }
   void safeFreeNode(std::atomic<Node *> nodeToFree) {
   //WIP
