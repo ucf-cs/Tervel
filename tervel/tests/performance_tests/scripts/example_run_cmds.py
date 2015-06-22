fcbfd274b3957e22b5bd5660d11c4fa4240db2ae
Steven Feldman <mr.stevenfeldman@gmail.com>
2015-06-22 15:09:47 -0400

    added time estimation to example run cmds and add a processing of logs file script

--- tervel/tests/performance_tests/scripts/example_run_cmds.py
+++ tervel/tests/performance_tests/scripts/example_run_cmds.py
@@ -1,40 +1,47 @@
 import time
 
+exe_count = 0;
+time_count = 0;
+cmds  = "dir=logs/$(date +\"%s\")\n"
+cmds += "mkdir $dir\n"
 
 threads = [1,2,4,8,16,32,64]
-exeTime = [20]
-
-def ds_test(exe, exe_config, distributions):
+exeTime = [5]
+path = "../executables/"
+def ds_test(exe, ds_config, distributions):
+    global exe_count, time_count, cmds
     for time in exeTime:
         for dist in distributions:
             for thread in threads:
-                print "temp=$dir/$(date +\"%s\").log"
-                cmd = ""
-                cmd += "./../executables/" + str(exe) + ".x -num_threads="+str(thread)
-                cmd += " -main_sleep=0  -execution_time="+str(time) + " " + exe_config
-                cmd += " " + str(thread) + " " + str(dist)
-                print "echo \"CMD : " + cmd + "\" > $temp"
-                cmd += " 2>&1 > $temp"
-                print "echo \"" + cmd + "\""
-                print cmd
+                exe_count += 1
+                time_count += time
+                cmds += "temp=$dir/$(date +\"%s\").log\n"
+                exe_cmd  = "./" + str(exe)
+                exe_cmd += " -main_sleep=0"
+                exe_cmd += "  -execution_time=" + str(time)
+                exe_cmd += " " + ds_config
+                exe_cmd += " -num_threads=" + str(thread)
+                exe_cmd += " " + str(thread) + " " + str(dist)
+                cmds += "echo \"CMD : " + exe_cmd + "\" > $temp\n"
+                exe_cmd += " 2>&1 > $temp"
+                cmds += "echo \"" + exe_cmd + "\"\n"
+                cmds += exe_cmd + "\n"
 
-def lfstack():
-    distributions = ["50 50"] #, "40 60", "60 40"]
-    ds_test("lf_stack", "-prefill=0", distributions)
-def wfstack():
-    distributions = ["50 50"] #, "40 60", "60 40"]
-    ds_test("wf_stack", "-prefill=0", distributions)
+def stack(exe):
+    prefills = [0, 1000]
+    for p in prefills:
+        distributions = ["50 50" , "40 60", "60 40"]
+        ds_test(path+exe, "-prefill=" + str(p), distributions)
+
 
 def wfhashmapnodel():
-    distributions = ["40 20 40"]
-    ds_test("wf_hashmap_nodel", "-prefill=0 -capacity=32568 -expansion_factor=6", distributions)
-
-
-
-print "dir=logs/$(date +\"%s\")"
-print "mkdir $dir"
-
+    distributions = ["40 20 40", "33 33 33", "20 40 40" "40 40 20"]
+    ds_test(path+"wf_hashmap_nodel.x", "-prefill=0 -capacity=32568 -expansion_factor=6", distributions)
 
 wfhashmapnodel()
-wfstack()
-lfstack()
+stack("wf_stack.x")
+stack("lf_stack.x")
+
+print "echo Number of Tests: " + str(exe_count)
+print "echo Estimated Time: " + str(time_count)
+print cmds
